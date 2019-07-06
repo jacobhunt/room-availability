e324bdabdb9dd15051671d1faaf669048807c094
Jacob Hunt <jacobhuntemail@gmail.com>
2019-07-06 16:55:31 -0400

    HTML updates and start of work on HTTP requests

--- UI-APP/flask-app.py
+++ UI-APP/flask-app.py
@@ -1,49 +1,56 @@
-# Name: t_flask
-# Author: Jacob Hunt
-# Author email: jacobhuntemail@gmail.com
-# Python version: 3.4.3
-#
-# Notes:
-# A simple implimentation of a flask-based Python server with some
-# minimalist CSS, JavaScript, and HTML form processing.  Will possibly
-# add an SQL database at some point in the future.
-
 import os
 from flask import Flask, render_template, request
 
+# class to represent rooms
+class Room:
+    def __init__(self, name, isOccupied):
+        self.name = str(name)
+        self.isOccupied = bool(isOccupied)
+
+# array to contain room instances
+rooms = []
+
+# object for python server application
 app = Flask(__name__)
 
 # set the secret key
-app.secret_key = "aLongStringOfRandomCharactersLike:_3n+k0(r37lb$mu..."
+app.secret_key = "8975917845yhiWUEFHYDFU*(#@!$73RYUIAWGDF"
 
 # root page
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# html form demo input page
-@app.route("/form")
-def form():
-    return render_template("form.html")
+# http route for setting status of a room (i.e. whether or not it is occupied) 
+@app.route("/setRoomStatus")
+def setRoomStatus():
 
-# html form demo result page
-@app.route("/result", methods = ["GET", "POST"])
-def result():
-    
+    # get remote input from an HTTP "post" request
     if request.method == "POST":
-        r_first = request.form.get("first")
-        r_middle = request.form.get("middle")
-        r_last = request.form.get("last")
+        req_name = str(request.form.get("name"))
+        req_isOccupied = bool(request.form.get("isOccupied"))
     
+    # get remote input from an HTTP "get" request
     if request.method == "GET":
-        r_first = request.args.get("first")
-        r_middle = request.args.get("middle")
-        r_last = request.args.get("last")
+        req_name = str(request.args.get("name"))
+        req_isOccupied = bool(request.args.get("isOccupied"))
+
+    # variable to indicate whether or not room already exists in "rooms" array
+    isRoomAlreadyInArray = False
     
-    return render_template("result.html",
-                           first = r_first,
-                           middle = r_middle,
-                           last = r_last)
+    # set the status for the room based on remote input if the room is already
+    # in the "rooms" array
+    for room in rooms:
+        if room.name == req_name:
+            room.isOccupied = req_isOccupied
+            isRoomAlreadyInArray = True
+
+    # add room and status to the "rooms" array if it is not already in array
+    if not isRoomAlreadyInArray:
+        rooms.append(Room(req_name, req_isOccupied))
+
+
+
 
 # configure server
 server_address = "0.0.0.0"
